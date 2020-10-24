127369d77355cece0c98c93f8cf8497ab58c3b07
Riley Crymble <rileyc2022@headroyce.org>
2020-10-24 12:37:35 -0700

    Repo reset

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 80
+
+app.use(express.static('public'));
+app.use('/cdn', require("./cdn"));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+
+
+// Last
+app.get('*', function(req, res){
+    res.status(404).sendFile(__dirname + '/404.html')
+  });
+
+app.listen(port, function () {
+    console.log(` Waiting for a friend on port ${port}`)
+})
